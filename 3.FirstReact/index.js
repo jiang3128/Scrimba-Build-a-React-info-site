6fb6bf38a9e8ff6728c47867f43d3646ccc78667
Jiang Zhu <zhuj3128@uwec.edu>
2022-04-18 21:03:33 -0500

    lesson 1 to lesson 5 from Scrimba

---
+++ 3.FirstReact/index.js
@@ -0,0 +1,6 @@
+// Spend a minute memorizing the line below
+ReactDOM.render(<h1>Hello, everyone!</h1>, document.getElementById("root"))
+// ReactDOM, React dom library in the CDM, we have a global variable
+// .render, it has a render method which allow us to place something on the screen
+// <h1>Hello, everyone!</h1> is the element we want to place inside of the div/container 
+// document.getElementById(), is select the place
